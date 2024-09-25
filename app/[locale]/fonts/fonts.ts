923720257418909b6a387ed6bd0eeaf5626aa80c
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2024-09-25 23:25:39 +0300

    chore(assests-files): add fonts and images of navbar and landingPage

---
+++ app/[locale]/fonts/fonts.ts
@@ -0,0 +1,7 @@
+import { Roboto, Work_Sans } from "next/font/google";
+
+export const workSans = Work_Sans({
+   subsets: ["latin"],
+  weight:["600",'400','500'],
+  variable:"--font-work-sans"
+  });
