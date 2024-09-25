2b46ba4ba861dde7cdad54b05077d6c4403dceb8
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2024-09-25 23:17:10 +0300

    chore(configuration): update config files

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'selector',
+  content: [   
+    './app/**/*.{js,ts,jsx,tsx,mdx}', 
+    './app/[locale]/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        workSans: ['var(--font-work-sans)'],
+      }
+    },
+  },
+  plugins: [],
+  darkMode :"class",
+}
+
