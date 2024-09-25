fac1618482fae4afff54f2a3b687e099e9413537
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2024-09-25 23:23:04 +0300

    feat(auth): add initial code of authentication using cookies

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
 // Objective: Handle authentication and authorization
 export async function login() {
 }
@@ -7,5 +10,20 @@ export async function login() {
 export async function logout() {
 }
 
-export async function register() {
+export async function register(formData: FormData) {
+    const username = formData.get('username');
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+ 
+
+  if (!username || !email || !password) {
+    return {
+      message: 'Please fill all fields',
+      success: false,
+    };
+  }
+
+  cookies().set('token', JSON.stringify({ username, email, password }));
+  redirect('/home');
 }
