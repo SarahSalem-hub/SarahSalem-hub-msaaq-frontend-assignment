f8bb404458843be97108cb4fba248108078294aa
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2024-09-25 23:19:11 +0300

    refactor(pages): change pages location

--- app/[locale]/blog/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Page() {
-    return <h1>
-        blog title
-    </h1>
-}
--- app/[locale]/blog/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function Page() {
-    return <ol>
-        <li>some blog title</li>
-        <li>some other blog title</li>
-    </ol>
-}
