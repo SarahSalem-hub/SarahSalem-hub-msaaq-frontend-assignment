738c413061e96c5530dd32a4b737132af13410e7
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2024-09-28 20:51:07 +0300

    feat(blogs-page): add posts to the page

--- app/[locale]/(main)/blog/page.tsx
+++ app/[locale]/(main)/blog/page.tsx
@@ -1,6 +1,21 @@
-export default function Page() {
-    return <ol>
-        <li>some blog title</li>
-        <li>some other blog title</li>
-    </ol>
+import Posts from "@/app/ui/Home/Posts/Posts";
+
+export default async function Page({
+    searchParams,
+    
+  }: {
+   
+    searchParams?: {
+      query?: string;
+      page?: string;
+    };
+  }) {
+    const query = searchParams?.query || "";
+    const currentPage = Number(searchParams?.page) || 1;
+    return (
+        <Posts 
+        query={query} 
+        page={currentPage}
+        />
+    )
 }
