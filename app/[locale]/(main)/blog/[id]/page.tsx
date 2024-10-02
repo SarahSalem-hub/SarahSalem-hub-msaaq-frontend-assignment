a98ab22984b2d1ac305568683804dc4427704b17
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2024-10-02 14:55:11 +0300

    feat(single-blog): modify page to display single blog post

--- app/[locale]/(main)/blog/[id]/page.tsx
+++ app/[locale]/(main)/blog/[id]/page.tsx
@@ -1,5 +1,21 @@
-export default function Page() {
-    return <h1>
-        blog title
-    </h1>
+import Wrapper from "@/app/ui/Wrapper";
+import { fetchSinglePosts } from "@/lib/fetchPosts";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  console.log("id", params.id);
+  const fetchedPost = await fetchSinglePosts(Number(params.id));
+  return (
+    <Wrapper>
+      <div className=" w-[80%] h-auto mt-[100px] flex flex-col gap-[40px] items-start rounded-[12px] border-[#E8E8EA] p-[16px] dark:border-[#242535] border-[2px] ">
+        <div className="flex flex-col gap-0">
+          <h1 className="text-[35px] ">Title</h1>
+          <h5 className="text-[25px]  ms-[24px]">{fetchedPost && fetchedPost.title}</h5>
+        </div>
+        <div>
+          <h1 className="text-[30px]">Blog</h1>
+          <p>{fetchedPost.body}</p>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
