06d753127ca9203cf9da6e6a6c6590c6e7948498
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2024-10-02 13:53:30 +0300

    feat(single-blog): add fetching single post using id

--- lib/fetchPosts.ts
+++ lib/fetchPosts.ts
@@ -21,3 +21,13 @@ export const generatePagination = (totalPages: number) => {
   const count = [...Array(totalPages)].map((_, index) => index + 1);
   return count;
 };
+
+
+export const fetchSinglePosts = async (postId: number) => {
+  const fetchAllPosts = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+  const resPosts = await fetchAllPosts.json();
+// console.log("resPosts",resPosts)
+  // const filteredPost = resPosts.find((post) => post.title.includes(query));
+  return resPosts
+
+}
