814be55a9fe3d41d90d58fd6ca9b782fb2a11e75
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2024-10-02 23:42:01 +0300

    feat(login): protect page if user logged in

--- app/[locale]/(auth)/login/page.tsx
+++ app/[locale]/(auth)/login/page.tsx
@@ -1,60 +1,72 @@
 import Wrapper from "@/app/ui/Wrapper";
-import { signIn } from "@/authConfig/auth";
+import { auth, signIn } from "@/authConfig/auth";
 import { AuthError } from "next-auth";
 import { useLocale, useTranslations } from "next-intl";
 import { redirect } from "next/navigation";
 import { getLangDir } from "rtl-detect";
 
 export default function Page() {
-  const t = useTranslations();
+  const session = auth();
 
+  const checkSession = () => {
+    if (session) {
+      redirect("/");
+    } else {
+      return <Login />;
+    }
+  };
+
+  <>{checkSession()}</>;
+}
+
+const Login = () => {
+  const t = useTranslations();
   const locale = useLocale();
   const direction = getLangDir(locale);
-
   return (
     <Wrapper>
-     <div className="text-[30px] ">{t("login.content")}</div>
-     <div className="w-[300px] h-[200px] flex justify-start items-center border-[2px]  dark:border-[#242535] rounded-[25px] px-[10px]">
-      <form
-        action={async (formData) => {
-          "use server";
-          try {
-            formData.append("redirectTo", "/");
-            await signIn("credentials", formData);
-          } catch (error) {
-            if (error instanceof AuthError) {
-              return redirect(`/error`);
+      <div className="text-[30px] ">{t("login.content")}</div>
+      <div className="w-[300px] h-[200px] flex justify-start items-center border-[2px]  dark:border-[#242535] rounded-[25px] px-[10px]">
+        <form
+          action={async (formData) => {
+            "use server";
+            try {
+              formData.append("redirectTo", "/");
+              await signIn("credentials", formData);
+            } catch (error) {
+              if (error instanceof AuthError) {
+                return redirect(`/error`);
+              }
+              throw error;
             }
-            throw error;
-          }
-        }}
-        className="w-full flex flex-col gap-[10px] "
-      >
-        <label
-          className={`flex flex-col  ${
-            direction === "ltr" ? "text-left" : "text-right"
-          }`}
+          }}
+          className="w-full flex flex-col gap-[10px] "
         >
-          {t("login.email")}
+          <label
+            className={`flex flex-col  ${
+              direction === "ltr" ? "text-left" : "text-right"
+            }`}
+          >
+            {t("login.email")}
 
-          <input
-            name="email"
-            type="text"
-            className="bg-[#F4F4F5] dark:bg-[#242535] rounded-[15px]"
-          />
-        </label>
-        <label className="flex flex-col">
-          {t("login.password")}
+            <input
+              name="email"
+              type="text"
+              className="bg-[#F4F4F5] dark:bg-[#242535] rounded-[15px]"
+            />
+          </label>
+          <label className="flex flex-col">
+            {t("login.password")}
 
-          <input
-            name="password"
-            type="password"
-            className="bg-[#F4F4F5] dark:bg-[#242535] rounded-[15px]"
-          />
-        </label>
-        <button type="submit">{t("auth.login")}</button>
-      </form>
-    </div>
+            <input
+              name="password"
+              type="password"
+              className="bg-[#F4F4F5] dark:bg-[#242535] rounded-[15px]"
+            />
+          </label>
+          <button type="submit">{t("auth.login")}</button>
+        </form>
+      </div>
     </Wrapper>
   );
-}
+};
