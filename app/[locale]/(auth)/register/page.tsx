fe42b320dd9f2dedf55e927f8036e85355bec956
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2024-10-02 14:48:39 +0300

    feat(register): add createUser functionality, add translation messages

--- app/[locale]/(auth)/register/page.tsx
+++ app/[locale]/(auth)/register/page.tsx
@@ -1,48 +1,42 @@
-import {register} from "@/lib/auth";
-import {useTranslations} from "next-intl";
-import { cookies } from "next/headers";
-
-
+import { createUser } from "@/lib/auth";
+import { useTranslations } from "next-intl";
 
 export default function Page() {
-    const t = useTranslations('auth');
-    
-    const cookieStore = cookies();
-    const token = cookieStore.get('token')?.value;
-    
-    return <form action={async (formData: FormData) => {
-        "use server";
-        await register(formData);
-        
-      }} className="flex flex-col">
-        <label>
-            Email
-            <input 
-            type="email"
-            name='email'
-            placeholder='Type here your email'
-            autoComplete='off'
-            required/>
-        </label>
-        <label>
-            Username
-            <input type="text"
-             name='username'
-             placeholder='Type here your username'
-             autoComplete='off'
-             required/>
-        </label>
-        <label>
-            Password
-            <input type="password"
-              name='password'
-              placeholder='Type here your password'
-              autoComplete='off'
-              required/>
-        </label>
-        <button type="submit">{t("register")}</button>
-        <div>
-            {token}
-        </div>
+  const t = useTranslations();
+
+  return (
+    <form action={createUser} className="flex flex-col">
+      <label>
+        {t("register.email")}
+        <input
+          type="email"
+          name="email"
+          placeholder={t("register.placeholders.email")}
+          autoComplete="off"
+          required
+        />
+      </label>
+      <label>
+        {t("register.username")}
+        <input
+          type="text"
+          name="username"
+          placeholder={t("register.placeholders.username")}
+          autoComplete="off"
+          required
+        />
+      </label>
+      <label>
+        {t("register.password")}
+        <input
+          type="password"
+          name="password"
+          placeholder={t("register.placeholders.password")}
+          autoComplete="off"
+          required
+        />
+      </label>
+      <button type="submit">{t("auth.register")}</button>
     </form>
+  );
 }
