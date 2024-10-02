16eee39347113737391f1c717778f46f365afaed
Sarah Salem <55980263+SarahSalem-hub@users.noreply.github.com>
2024-10-02 14:52:26 +0300

    chore(homepage): use wrapper container for the page, get session for displaying if user logged in

--- app/[locale]/(main)/page.tsx
+++ app/[locale]/(main)/page.tsx
@@ -1,14 +1,8 @@
 // import { cookies } from "next/headers";
 import HeroSection from "@/app/ui/Home/HeroSection";
 import Posts from "@/app/ui/Home/Posts/Posts";
-// import HomePage from "./(main)/home/page";
-// import { redirect } from "next/dist/server/api-utils";
-
-// const getToken = () => {
-//   const token = cookies().get("token")?.value;
-
-//   return token;
-// };
+import Wrapper from "@/app/ui/Wrapper";
+import { auth } from "@/authConfig/auth";
 
 export default async function Home({
   searchParams,
@@ -20,32 +14,15 @@ export default async function Home({
 }) {
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
-  // const token = getToken();
-
-  // const cookieStore = cookies();
-  // const userInfo = cookieStore.get("token")?.value;
-
-  // let parsedUserInfo;
-  // try {
-  //   parsedUserInfo = userInfo ? JSON.parse(userInfo) : null; // Parse the JSON string
-  // } catch (error) {
-  //   console.error("Error parsing user info:", error);
-  // }
-  // if (token) {
-  //   redirect("/home")
-  // }
+  const session = await auth(); //TODO: remove later
+  console.log("session", session);
 
   return (
-    <div className="w-full  flex justify-center dark:bg-[#181A2A] bg-[#FFFFFF]">
-      <div className=" w-[1216px]">
-        <HeroSection />
-        <Posts
-          // postsPages={postsPages}
-          query={query}
-          page={currentPage}
-          // fetchedPosts={posts}
-        />
-      </div>
-    </div>
+    <Wrapper>
+      <div>{JSON.stringify(session, null, 2)}</div>
+
+      <HeroSection />
+      <Posts query={query} page={currentPage} />
+    </Wrapper>
   );
 }
